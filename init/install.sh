c865f6049274fd0a2c6c8e7c90f0c2cfd6550834
HorusElohim <HorusElohim@gmail.com>
2021-03-14 15:49:07 +0100

    Fix install script

--- init/install.sh
+++ init/install.sh
@@ -1,33 +1,8 @@
 #! /bin/bash
 
 # Install git if needed
-sudo apt install -y git
-
-# Create Mark WS
-sudo mkdir /mark
-sudo chown -R mark: /mark
-pushd /mark
-	pushd git
-		# Create git folder structure
-		mkdir -p /mark/git/{horuselohim,mark,akka}
-
-		# Install Libs
-		pushd horuselohim
-			git clone https://github.com/HorusElohim/zsh-helo-installer.git
-			pushd zsh-helo-installer
-				pushd shell
-					# Install Zsh
-					sudo ./zsh.sh
-					./oh_zsh.sh
-				popd
-			popd
-		popd
-	popd
-
-	# Install MarkPy
-	pushd mark
-		git clone https://github.com/MarkNo1/MarkPy.git
-	popd
-
-
+sudo apt install -y git zenity zsh
+git clone https://github.com/HorusElohim/zsh-helo-installer.git
+pushd zsh-helo-installer
+  ./main.sh
 popd
