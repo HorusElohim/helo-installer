db21211b50f60fe45209e9b35535806426ce2fac
HorusElohim <74312168+HorusElohim@users.noreply.github.com>
2021-11-26 18:29:21 +0100

    Undo wrong change.

--- zenity/main.sh
+++ zenity/main.sh
@@ -15,8 +15,7 @@ mkdir -p "$cache_date_dir"
 
 # Select the script to install
 tell "Now select the scripts you want install with the checkbox"
-# selected_sh=$(./zenity_list.sh)
-selected_sh=$DIR
+selected_sh=$(./zenity_list.sh)
 
 IFS='|' #setting delimiter
 read -a scripts_sel_splitted <<<"$selected_sh" #reading str as an array as tokens separated by IFS
